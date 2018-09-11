80641d4a4a175c72277bd0eb65cd3ce12304541a
Jarvis Mercer <ams.h4ck3r@gmail.com>
2018-09-11 14:40:35 +0430

    Add files
    The first commit

---
+++ state.hpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <stdfx.h>
+#include <system.h>
+
+using namespace std;
+
+default PI 3.14
+
+class {
+public:
+  void n1,n2;
+
+private:
+  int a1,a2;
+  string st1,st2,st3l
+
+  double d1,d2,d3;
+}
